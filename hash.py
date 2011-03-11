44c1f1da766bf02300821c6320aa0ccb37862175
C. Titus Brown <titus@idyll.org>
2011-03-11 18:17:24 -0500

    k up to 16; prime generator

--- hash.py
+++ hash.py
@@ -1,18 +1,51 @@
 import string
 
+MAX_K=16
+DEFAULT_K=8
+
+def is_prime(n):
+   '''
+   checks if a number is prime
+   '''
+   if n < 2:
+      return False
+   if n == 2:
+      return True
+   if n % 2 == 0:
+      return False
+   for x in range(3, int(n**0.5)+1, 2):
+      if n % x == 0:
+         return False
+   return True
+
+def get_n_primes_above_x(n, x):
+   '''
+   steps forward until n primes (other than 2) have been
+   found that are smaller than x.
+   '''
+   primes = []
+   i = x+1
+   if i % 2 == 0:
+      i += 1
+   while len(primes) != n and i > 0:
+      if is_prime(i):
+         primes.append(i)
+      i += 2
+   return primes
+
 def hash(word):
-    assert len(word) <= 8
+    assert len(word) <= MAX_K
 
     value = 0
     for n, ch in enumerate(word):
-        value += ord(ch) * 256**n
+        value += ord(ch) * 128**n
 
     return value
 
 class BloomFilter(object):
-    allchars = "".join([ chr(i) for i in range(256) ])
+    allchars = "".join([ chr(i) for i in range(128) ])
     
-    def __init__(self, tablesizes, k=8):
+    def __init__(self, tablesizes, k=DEFAULT_K):
         self.tables = [ (size, [0] * size) for size in tablesizes ]
         self.k = k
 
@@ -61,17 +94,17 @@ def next_words(bf, word):
             yield ch
         
 
-def retrieve_all_sentences(bf, start):
+def retrieve_all_sentences(bf, start, max_depth=50):
+    if max_depth == 0:
+        yield start + '...'
+        return
+        
     word = start[-bf.k:]
 
     n = -1
     for n, ch in enumerate(next_words(bf, word)):
-        for sentence in retrieve_all_sentences(bf, start + ch):
+        for sentence in retrieve_all_sentences(bf, start + ch, max_depth-1):
             yield sentence
 
     if n < 0:
         yield start
-        
-
-if __name__ == '__main__':
-    pass
